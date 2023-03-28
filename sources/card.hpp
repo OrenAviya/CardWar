427f917e5808981e83adf17be9ae309ce6b604d8
aviya oren <98823130+OrenAviya@users.noreply.github.com>
2023-03-28 15:24:07 +0300

    with card class

--- sources/card.hpp
+++ sources/card.hpp
@@ -9,4 +9,27 @@ using namespace std;
 #include "player.hpp"
 namespace ariel{}
 using namespace ariel;
+
+class Card{
+
+private:
+    string color;
+    int number;
+    string shape;
+public:
+
+// constructor:
+Card (int n , string shape , string col){
+this->number = n;
+this->color = col;
+this->shape = shape;
+}
+
+ void printCard();
+ int getNum();
+ string getShape();
+ string getColor();
+};
+
+
 //#endif Card

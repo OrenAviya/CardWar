12fcb41b08a45068440274b9a7e162a076e1010a
aviya oren <98823130+OrenAviya@users.noreply.github.com>
2023-04-09 15:07:23 +0300

    getting better

--- sources/card.cpp
+++ sources/card.cpp
@@ -4,67 +4,8 @@
 #include <sstream>
 #include <stdexcept>
 #include "card.hpp"
-#include "player.hpp"
+//#include "player.hpp"
 
 using namespace std;
 
 
-void Card::printCard(){
-   string res="";
-   if (this->number > 10 || this->number ==1){
-    switch(this->number){
-    case 11:
-        res+= "Jack";
-        break;
-    case 12:
-        res+= "Queen";
-        break;
-    case 13:
-        res+= "King";
-        break;
-    case 1:
-        res += "Ace";
-        break;
-   }
-   }
-   else {
-   switch(this->number){
-    case 2:
-        res+= "Two";
-        break;
-    case 3:
-        res+= "Three";
-        break;
-    case 4:
-        res+= "Four";
-        break;
-    case 5:
-        res += "Five";
-        break;
-    case 6:
-        res+= "Six";
-        break;
-    case 7:
-        res+= "Sevem";
-        break;
-    case 8:
-        res+= "Eight";
-        break;
-    case 9:
-        res+= "Nine";
-        break;
-   }
- }
-res+= "of";
-// needed to add the shape and the color
-   cout<< res;
-}
- int Card::getNum(){
-    return this->number;
- }
- string Card::getShape(){
-    return this->shape;
- }
- string Card:: getColor(){
-    return this->color;
- }
--- sources/game.hpp
+++ sources/game.hpp
@@ -2,12 +2,14 @@
 // #define GAME 
 #pragma once
 
+#include <vector>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <stdexcept>
 using namespace std;
 #include "player.hpp"
+#include "card.hpp"
 namespace ariel{}
 using namespace ariel;
 
@@ -15,6 +17,12 @@ class Game{
     private: 
         Player p1{};
         Player p2{};
+        std:: vector <Card> cards_;
+        //add something that help to know how won last turn
+        string LastTurn;
+        vector <string> winners;
+        vector<string> AllTurns;
+        int num_of_draws;
     
     public:
 //constructors
@@ -23,14 +31,47 @@ Game (Player pl1 , Player pl2) :
     p2{std::move(pl2)}
   {
 //std :: cout << "new game with : " + std::to_string(p1)+" "+ std::to_string(p2) << std::end1;
+ // Initialize the deck with 52 cards
+        for (int suit = Card::Hearts; suit <= Card::Spades; ++suit) {
+            for (int rank = Card::Ace; rank <= Card::King; ++rank) {
+                cards_.push_back(Card(static_cast<Card::Suit>(suit), static_cast<Card::Rank>(rank)));
+          }
+        }
+// Shuffle the deck
+        std::random_shuffle(this->cards_.begin(), this->cards_.end());
+
+ // Dealing the cards to the two players
+    for (int i=0; i<cards_.size()-1  ; i+=2)
+    {
+      Card c1 = static_cast<Card>(cards_[static_cast<size_t>(i)]);
+      Card c2 = static_cast<Card>(cards_[static_cast<size_t>(i+1)]);
+      //p1.setStackValuesAt(i)
+      p1.GetStack().push_back(c1); 
+      p2.GetStack().push_back(c2);
+
+    }
+    // p1.stacksize() = p1.GetStack().size();
+    // p2.stacksize() = p2.GetStack().size();
+    p1.SetStacksize(p1.GetStack().size());
+    p2.SetStacksize(p2.GetStack().size());
+  cout << "stacks sizes: \n" << p1.stacksize();
+  cout << "\n"<< p2.stacksize()<<"\n";
+    this->LastTurn = "";
+    this->num_of_draws= 0;
+    
   }  
+
 void playTurn();
-string printLastTurn();
+void printLastTurn();
 
 void playAll();
 void printWiner(); 
 void printLog();
 void printStats();
+void AddToWinners (string name);
+vector<string>& getWinners();
+string getLastTurn();
+int getdraws();
 
 };
 
